3aac4d76e52c4fa0e96701465e30758cf9954aea
Michael Dzura <mdzura@Michael-Dzuras-2010-Mac-Pro.local>
2020-03-31 15:06:56 -0400

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
+import { AboutComponent } from './about.component';
+import { ContactComponent } from './contact.component';
+import { TermsComponent } from './terms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    TermsComponent
   ],
   imports: [
     BrowserModule,
