8fb1e7fc66f8260bb9e6ce8abf1db8710d15bb1b
Michael Dzura <mdzura@Michael-Dzuras-2010-Mac-Pro.local>
2020-03-31 23:41:13 -0400

    more updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from "./home.component";
+import { AboutComponent } from "./about.component";
+import { ContactComponent } from "./contact.component";
+import { TermsComponent } from './terms.component';
+import { NotfoundComponent } from "./notfound.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'terms', component: TermsComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: NotfoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
